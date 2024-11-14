ff2568337f24323de2517f02b5849d8716e123f3
Luis <luisfelipechristogomes@gmail.com>
2024-11-14 19:32:14 -0300

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,8 @@
+public class Ex1 {
+    public static void main(String[]args) {
+        int numero = 5;
+        String texto = "10";
+        int resultado = numero + Integer.parseInt(texto);
+        System.out.println("O resultado é :" + resultado);
+    }
+}
---
+++ Ex2.java
@@ -0,0 +1,9 @@
+public class Ex2 {
+    public static void main(String[]args) {
+        int variavel = 10;
+        String texto = "Texto";
+        System.out.println("Valor da variavel inteira: " + variavel);
+        System.out.println("Valor da variavel texto: " + texto);
+    }
+}
+ 
