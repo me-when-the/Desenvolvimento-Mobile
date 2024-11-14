56f8f916892bfa8e21383793a91d5e0b6559351f
Luis <luisfelipechristogomes@gmail.com>
2024-11-14 20:32:49 -0300

    Add files via upload

---
+++ ExemploFor.java
@@ -0,0 +1,10 @@
+//utilizado quando o número de repetições é conhecido
+//Imprime nº de 1 a 10
+
+public class ExemploFor {
+    public static void main(String[] args){
+        for (int i=1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
